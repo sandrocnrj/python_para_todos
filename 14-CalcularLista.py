01063f589cb4c4589f26f535e6401643a084c0d0
Sandro Carvalho <sandro.prog@gmail.com>
2014-11-23 18:54:26 -0200

    Python Para Todos

---
+++ 14-CalcularLista.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Arquivo CalcularLista.py
+
+"""
+    Módulo que contém duas funcções
+    Uma que calcula a média de uma lista
+    E outra que calcula a soma de uma lista
+"""
+
+def media(lista):
+    """Calcula a média dos itens de uma lsita"""
+    return float(sum(lista))/len(lista)
+
+def somalista(lista):
+    """Calcula a soma dos itens de uma lista"""
+    return sum(lista)
---
+++ 15-testandoModulo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#import CalcularLista
+#from CalcularLista import media, somalista
+from CalcularLista import *
+
+lista = [5,10,15,20,25,30,35,40,45,50]
+
+#print(CalcularLista.media(lista))
+#print(CalcularLista.somalista(lista))
+
+#print(CalcularLista.__doc__)
+
+print(media(lista))
+print(somalista(lista))
