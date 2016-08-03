07de307549162d04d2bf6a1bb6364503285f1662
Sandro Carvalho <sandro.prog@gmail.com>
2016-08-02 22:44:50 -0300

    Python Para Todos: MongoDB e CSV

---
+++ mongodb_csv.py
@@ -0,0 +1,19 @@
+import pymongo
+import csv
+import os
+
+conn = pymongo.MongoClient()
+
+db = conn.pythondb
+
+cursor = db.pessoas.find({},{'_id':1, 'nome': 1})
+
+with open('pessoas.csv', 'w') as outfile:
+    fields = ['_id','nome']
+    writer = csv.DictWriter(outfile, fieldnames=fields)
+    writer.writeheader()
+    for x in cursor:
+        writer.writerow(x)
+
+# close file
+outfile.close()
